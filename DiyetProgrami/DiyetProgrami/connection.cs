618d58b970a53b79e88bfc8446628a0886e97aaf
MrHrn <oyuncumarasli@gmail.com>
2026-05-17 20:23:42 +0300

    .

--- DiyetProgrami/DiyetProgrami/connection.cs
+++ DiyetProgrami/DiyetProgrami/connection.cs
@@ -12,7 +12,7 @@ namespace DiyetProgrami
         
          
         public static string connectionstring { get; set; } = ConfigurationManager.ConnectionStrings["DiyetProgramiConnectionString"].ConnectionString;
-        //Sağda bulunan dbveritabani.mdf dosyasına çif tıklarsan data source görüceksin. onu alıp tırnak içinde yapıştırdığım yere yapıştır.
+        
 
     }
 }
